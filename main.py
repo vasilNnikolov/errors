80fcc41ed3dc259a4903988d87a41e184bbca8d8
vasil <vaskonikolov2003@gmail.com>
2021-12-03 12:56:25 +0200

    arg parsing without keywords

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 class Variable(float):
     """define a variable with value and error kwargs or function and parameters kwargs(parameters is a tuple of Variable objects)"""
-    def __new__(self, **kwargs):
-        _, value = self.__get_define_type_value(self, kwargs)
+    def __new__(self, *args):
+        _, value = self.__get_define_type_value(self, args)
         return float.__new__(self, value)
-    
-    def __init__(self, **kwargs):
-        self.__define_type, self.value = self.__get_define_type_value(kwargs)
-        self.__kwargs = kwargs
-    
+
+    def __init__(self, *args):
+        define_type, value = self.__get_define_type_value(args)
+        self.__define_type = define_type
+        self.value = value
+        if self.__define_type == "value":
+            self.__error = float(args[1])
+        elif self.__define_type == "function":
+            self.__function = args[0]
+            self.__parameters = args[1]
+
     def get_std_error(self):
         if self.__define_type == "value":
-            return float(self.__kwargs["error"])
+            return self.__error 
         else:
             # compute error from standard deviations
-            n_params = len(self.__kwargs["parameters"])
-            parameter_errors = [self.__kwargs["parameters"][i].get_std_error() for i in range(n_params)]
+            n_params = len(self.__parameters)
+            parameter_errors = [self.__parameters[i].get_std_error() for i in range(n_params)]
             return (sum([(self.__partial_derivative(i)*parameter_errors[i])**2 for i in range(n_params)])) ** 0.5
 
     def __partial_derivative(self, argument_index):
         if self.__define_type != "function":
             raise Exception("cannot compute partial derivative of variable defined with value and error")
         else:
-            function = self.__kwargs["function"]
-            args = self.__kwargs["parameters"]
+            function = self.__function
+            args = self.__parameters
             modified_args = list(args)
             if modified_args[argument_index] != 0:
                 increment = 0.001*modified_args[argument_index]
@@ -33,39 +39,47 @@ class Variable(float):
                 modified_args[argument_index] += increment
                 return (function(*modified_args) - function(*args))/increment
 
-
-
-    def __get_define_type_value(self, kwargs):
+    def __get_define_type_value(self, args):
         """returns either "value" or "function", depending on the definition type"""
+        if len(args) != 2:
+            raise Exception("Constructor accepts either value and error of function and its arguments as args")
+
         # parse value and error definition
-        if len(kwargs) != 2:
-            raise Exception("Constructor accepts either value and error of function and its arguments as named arguments(kwargs)")
-
-        if "value" in kwargs and "error" in kwargs:
+        def is_float(v):
             try:
-                value = float(kwargs["value"])
-                error = float(kwargs["error"])
-
-                return ("value", value)
-            except ValueError:
-                raise Exception("Value or error given in wrong format")
+                v = float(v)
+                return True
+            except Exception:
+                return False
         
+        if is_float(args[0]) and is_float(args[1]):
+            value = float(args[0])
+            error = float(args[1])
+
+            return ("value", value)
+
         #parse function and variables definition
-        elif "function" in kwargs and "parameters" in kwargs:
-            if any(not isinstance(param, Variable) for param in kwargs["parameters"]):
+        elif callable(args[0]) and type(args) is tuple:
+            if any(not isinstance(param, Variable) for param in args[1]):
                 raise Exception("The arguments to the function must be of class Variable")
             else:
                 try:
-                    function = kwargs["function"]
-                    value = float(function(*kwargs["parameters"]))
+                    function = args[0] 
+                    value = float(function(*args[1]))
 
                     return ("function", value)
                 except TypeError:
                     raise Exception("The number of variables given does not equal the number of variables the given function accepts")
+        
+        else:
+            raise Exception("Pass either two floats(value and error) or callable function and its arguments in a tuple")
                 
 
 if __name__ == "__main__":
-    function = (lambda x, y: x**2*y**3)
-    params = (Variable(value=10, error=0.1), Variable(value=3, error=0.1))
-    b = Variable(function=function, parameters=params)
-    print(b, b.get_std_error())
+    a = Variable(10, 0.1)
+    b = Variable(5, 0.2)
+    c = Variable(30, 0.75)
+    print(c.__error)
+
+    d = Variable(lambda x, y, z: (x**2 * y**5)/z, (a, b, c))
+    print(d, d.value, d.get_std_error())
